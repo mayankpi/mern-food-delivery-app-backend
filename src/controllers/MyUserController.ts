f6d716206e86f080f9833e81fe289e01860112fc
mayankpi <mayankm522@gmail.com>
2024-05-15 00:37:24 +0530

    add update user api

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -2,17 +2,12 @@ import { Request, Response } from "express";
 import User from "../model/user";
 
 const createCurrentUser = async (req: Request, res: Response) => {
-    // TODO:
-    // 1. check if user exists
-    // 2. create the user if it doesn't exist
-    // 3. return the user object to the calling client
-
     try {
         const { auth0Id } = req.body;
         const existingUser = await User.findOne({ auth0Id });
 
         if (existingUser) {
-            return res.status(409).json({ message: "User already exists" });
+            return res.status(200).send();
         }
 
         const newUser = new User(req.body);
@@ -25,6 +20,30 @@ const createCurrentUser = async (req: Request, res: Response) => {
     }
 };
 
+const updateCurrentUser = async (req: Request, res: Response) => {
+    try {
+        const { name, addressLine1, country, city } = req.body;
+        const user = await User.findById(req.userId);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        user.name = name;
+        user.addressLine1 = addressLine1;
+        user.city = city;
+        user.country = country;
+
+        await user.save();
+
+        res.send(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error updating user" });
+    }
+};
+
 export default {
     createCurrentUser,
+    updateCurrentUser,
 };
