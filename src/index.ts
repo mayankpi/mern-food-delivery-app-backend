dda7aede75262d347550857f424259ebcd962c8a
mayankpi <mayankm522@gmail.com>
2024-05-17 01:30:16 +0530

    add helmet

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,29 @@ import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
 import orderRoute from "./routes/OrderRoute";
+const helmet = require("helmet");
 
 mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("Connected to database!"));
+    .connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => console.log("Connected to database!"));
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
 app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
-  res.send({ message: "health OK!" });
+    res.send({ message: "health OK!" });
 });
 
 app.use("/api/my/user", myUserRoute);
@@ -36,5 +38,5 @@ app.use("/api/restaurant", restaurantRoute);
 app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
-  console.log("server started on localhost:7000");
+    console.log("server started on localhost:7000");
 });
